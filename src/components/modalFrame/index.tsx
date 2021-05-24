3df4752426c2694a4978cfa9518af75fb875051e
gibercode <giberlara@gmail.com>
2021-05-24 09:07:04 -0400

    modal fram progress

---
+++ src/components/modalFrame/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const ModalFrame = ({ children }) => (
+  <div className={styles._parent}>
+    <div className={styles._modal}>
+    <h1>hey</h1>
+    </div>
+  </div>
+)
+
+export default ModalFrame;
