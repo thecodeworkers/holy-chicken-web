b5ceb306b257d61b47e861d207e60d01c7170b4b
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-06-01 20:57:12 -0400

    testing

--- src/components/AboutUs/elements/FirstBanner/index.tsx
+++ src/components/AboutUs/elements/FirstBanner/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import styles from './styles.module.scss'
 import { GeneralCard, Button, Stepper } from '@components'
 import { useRouter } from 'next/router'
+import { Timeline } from 'antd';
 
 const FirstBanner = ({ data }) => {
 
@@ -13,11 +14,49 @@ const FirstBanner = ({ data }) => {
   return (
 
     <div className={styles._content}>
-      <div style={{display:'flex'}}>
+      <div className={styles._road}>
 
+        {
+          data?.road.map((item, index) => {
 
+            return (
+              <div className={styles._roadContainer} key={index}>
+                <div className={index === 0 ? styles._lineFather : styles._lineFatherTwo}>
+                  <div className={index === 0 ? styles._lineTwo : styles._line}> </div>
+                </div>
 
-         </div>
+                {index % 2 == 0 ?
+                  <>
+                    <div className={styles._imageLeftContainer}>
+                      <img className={styles._image} src={item?.image.mediaItemUrl} />
+                    </div>
+
+                    <div className={styles._textLeftContainer}>
+                      {index == 0 ? <p className={styles._title}>{data.title}</p> : null}
+                      <p className={styles._textContent}>{item?.content}</p>
+                    </div>
+                  </>
+                  :
+                  <>
+                    <div className={styles._textRightContainer}>
+                      <p className={styles._textContent}>{item?.content}</p>
+                    </div>
+
+                    <div className={styles._imageRightContainer}>
+                      <img className={styles._image} src={item?.image.mediaItemUrl} />
+                    </div>
+
+                  </>
+                }
+
+              </div>
+              //   </div>
+              // </div>
+            )
+          }
+          )
+        }
+      </div>
     </div>
   )
 }
