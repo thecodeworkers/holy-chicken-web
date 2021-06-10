b0904d5f4a65f79aacd084e079e8722fff9cdc7d
gibercode <giberlara@gmail.com>
2021-06-10 01:48:18 -0400

    persist user data

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { HYDRATE, createWrapper } from 'next-redux-wrapper'
 import thunkMiddleware from 'redux-thunk'
 import reducers from './reducers'
 import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
+import reconcile from './reconcile'
 
 const createNoopStorage = () => {
   return {
@@ -28,8 +29,11 @@ const bindMiddleware = (middleware) => {
 }
 
 const reducer = (state, action) => {
-  if (action.type === HYDRATE) return { ...state, ...action.payload }
-  if (action.type === 'persist/REHYDRATE') action.payload = { ...action.payload, ...state }
+  let reconcileState = {}
+
+  if (action.type == '__NEXT_REDUX_WRAPPER_HYDRATE__') reconcileState = reconcile(state, action.payload)
+  if (action.type === HYDRATE) return { ...state, ...action.payload, ...reconcileState }
+
   return reducers(state, action)
 }
 
---
+++ src/store/reconcile.ts
@@ -0,0 +1,44 @@
+const reconcile = (state, payload) => {
+  const keyNames = Object.keys(state);
+  let trueState = {}
+
+  keyNames.forEach(key => {
+    switch (key) {
+      case 'page':
+        let currentPages = state[key]
+        const rewritePages = {}
+
+        for (let currentPage in currentPages) {
+          const prevPage = currentPages[currentPage]
+          const existPrevPage = Object.keys(prevPage).length
+
+          if (existPrevPage) {
+            rewritePages[currentPage] = prevPage
+
+            trueState = {
+              ...trueState,
+              page: {
+                ...currentPages,
+                ...payload[key],
+                ...rewritePages
+              }
+            }
+          }
+        }
+
+        break
+
+      case 'auth':
+        const credentials = state[key]
+        trueState = { ...trueState, auth: credentials }
+        break
+
+      default:
+        break
+    }
+  });
+
+  return trueState
+}
+
+export default reconcile
