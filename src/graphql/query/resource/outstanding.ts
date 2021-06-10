cfd3258da8f352f62934fd344549325861c08b40
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-06-10 18:50:47 -0400

    test

---
+++ src/graphql/query/resource/outstanding.ts
@@ -0,0 +1,39 @@
+
+const outstanding = `
+products (where: {categoryId: 42) {
+  nodes {
+    id
+    description
+    slug
+    ... on SimpleProduct {
+      id
+      name
+      price
+      attributes {
+        nodes {
+          id
+          label
+          name
+          options
+        }
+      }
+      image {
+        mediaItemUrl
+        slug
+        description
+      }
+    }
+    productCategories {
+      nodes {
+        id
+        name
+        slug
+        productCategoryId
+      }
+    }
+  }
+}
+}
+`
+
+export default outstanding
