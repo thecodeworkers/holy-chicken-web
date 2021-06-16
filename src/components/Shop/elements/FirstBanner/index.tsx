f61ffb5d0747d860860f6d8fba9e9b85ae5fd2a9
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-06-15 21:50:08 -0400

    adjustments

--- src/components/Shop/elements/FirstBanner/index.tsx
+++ src/components/Shop/elements/FirstBanner/index.tsx
@@ -4,7 +4,7 @@ import { GeneralCard } from '@components'
 import { useRouter } from 'next/router'
 import { Search } from '@images/icons';
 
-const FirstBanner = ({content }) => {
+const FirstBanner = ({ content }) => {
 
   const router = useRouter()
 
@@ -23,20 +23,44 @@ const FirstBanner = ({content }) => {
 
         <div className={styles._shopContainer}>
           <div className={styles._filterContainer}>
-            <p className={styles._filterTitle}>filtro</p>
-          </div>
-          <div className={styles._productContainer}>
-          <div className={styles._inputParent}>
-            <input
-              placeholder='Pide tu deseo…'
-              name='password'
-              className={styles._input} />
-            <div className={styles._imageParent} >
-            <Search color={'#000000'} />
+          <div className={styles._container}>
+            <div className={styles._row}>
+              <p className={styles._littleTitle}>Desayunos</p>
+              <div className={styles._checkParent}>
+                <input type='radio' className={styles._radioBtn} defaultChecked={false}></input>
+              </div>
+            </div>
+            <div className={styles._row}>
+              <p className={styles._littleTitle}>Holy Sánduches</p>
+              <div className={styles._checkParent}>
+                <input type='radio' className={styles._radioBtn} defaultChecked={false}></input>
+              </div>
+            </div>
+            <div className={styles._row}>
+              <p className={styles._littleTitle}>Holy Tenders</p>
+              <div className={styles._checkParent}>
+                <input type='radio' className={styles._radioBtn} defaultChecked={false}></input>
+              </div>
+            </div>
             </div>
           </div>
+          <div className={styles._productContainer}>
+            <div className={styles._searchContainer}>
+            <div className={styles._inputParent}>
+              <input
+                placeholder='Pide tu deseo…'
+                name='search'
+                className={styles._input} />
+              <div className={styles._imageParent} >
+                <Search color={'#000000'} />
+              </div>
+            </div>
+            <div className={styles._dropDown}>
+              <p>Ordernar por</p>
+            </div>
+            </div>
             <div className={styles._cardContainer}>
-            {
+              {
                 content.map((item, index) => {
                   return (
                     <div className={styles._card} key={index}>
