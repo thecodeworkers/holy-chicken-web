ebfac7483b2d48b9402250cd98d47698ea3ca82d
giber <giberlara@gmail.com>
2021-06-24 09:09:29 -0400

    cart changes

--- src/store/cart/action.ts
+++ src/store/cart/action.ts
@@ -15,10 +15,10 @@ export const setCartProducts = ({ databaseId, quantity }) => async (dispatch, ge
       if(result.message) throw new Error(result.message)
 
       const { addCartItems } = result
+      const itemsNumber = addCartItems?.cart?.contents?.itemCount
 
-      console.log('NUMERO DE ITEEEMS', addCartItems?.cart?.contents?.itemCount)
       dispatch(actionObject(CART_PRODUCTS, { cartProducts: addCartItems?.cart }))
-      dispatch(setProductsNumber({ number: addCartItems?.cart?.contents?.itemCount }))
+      dispatch(setProductsNumber({ number: itemsNumber }))
     }
 
   } catch(error) {
