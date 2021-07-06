fba79213621a3ef300d5ddf24b4d58e6ac375734
giber <giberlara@gmail.com>
2021-07-06 10:08:58 -0400

    stepper changes

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,14 +1,65 @@
+import { useState } from 'react'
 import { Navbar } from '@components'
 import Head from 'next/head'
+import styles from './styles.module.scss'
+import { Chiken } from '@images/resources'
 
-const History = ({ data }) => (
-  <>
-    <Head>
-      <title>Summary</title>
-    </Head>
-    <Navbar data={data?.header} />
-    <div>History!</div>
-  </>
-)
+const circles = [0, 1, 2, 3]
+
+const History = ({ data }) => {
+
+  const [currentStep, setCurrentStep] = useState(1)
+
+  return (
+    <>
+      <Head>
+        <title>History</title>
+      </Head>
+      <Navbar data={data?.header} />
+      <div className={styles._parent}>
+
+        <div className={styles._leftSide}>
+          <div className={styles._infoParent}>
+            <ul className={styles._list}>
+              <li>Nombre y apellido</li>
+              <li>email@email.com</li>
+              <li>000-00000000</li>
+            </ul>
+          </div>
+
+          <div className={styles._orderParent}>
+            <p>Orden</p>
+            <input className={styles._input} readOnly value='#000'></input>
+          </div>
+          <div className={styles._line}>
+            <div className={styles._pointsParent}>
+              <div className={styles._circles}>
+                {
+                  circles.map((res, index) => {
+
+                    const itemIndex = index + 1
+                    return (
+                      <div className={itemIndex == currentStep ? styles._circleSelected : styles._circle} key={index}>
+                        {
+
+                          itemIndex == currentStep &&
+                          <div className={styles._chickenParent}>
+                            <Chiken />
+                          </div>
+                        }
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+
+}
 
 export default History
