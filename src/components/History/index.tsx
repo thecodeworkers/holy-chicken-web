f919a28ef7b13fb85dc8e5a5d6076f439457aa10
giber <giberlara@gmail.com>
2021-07-07 00:17:53 -0400

    history lay out progress

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,14 +1,69 @@
 import { useState } from 'react'
-import { Navbar } from '@components'
+import { Navbar, Button, Footer } from '@components'
 import Head from 'next/head'
 import styles from './styles.module.scss'
 import { Chiken } from '@images/resources'
+import { Search } from '@images/icons';
 
-const circles = [0, 1, 2, 3]
+const circles = [
+  { label: 'Preparando pedido' },
+  { label: 'Por entregar' },
+  { label: 'Orden en vía' },
+  { label: 'Orden Entregada' }
+]
+
+const orders = [
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  },
+  {
+    number: '#000',
+    date: '00/00/00',
+    hour: '00:00PM'
+  }
+]
+
 
 const History = ({ data }) => {
 
-  const [currentStep, setCurrentStep] = useState(1)
+  const [currentStep, setCurrentStep] = useState(4)
 
   return (
     <>
@@ -19,44 +74,123 @@ const History = ({ data }) => {
       <div className={styles._parent}>
 
         <div className={styles._leftSide}>
-          <div className={styles._infoParent}>
-            <ul className={styles._list}>
-              <li>Nombre y apellido</li>
-              <li>email@email.com</li>
-              <li>000-00000000</li>
-            </ul>
+          {/* <div className={styles._secondParent}> */}
+
+          <div>
+            <div className={styles._infoParent}>
+              <ul className={styles._list}>
+                <li>Nombre y apellido</li>
+                <li>email@email.com</li>
+                <li>000-00000000</li>
+              </ul>
+            </div>
+
+            <div className={styles._orderParent}>
+              <p>Orden</p>
+              <input className={styles._input} readOnly value='#000'></input>
+            </div>
           </div>
 
-          <div className={styles._orderParent}>
-            <p>Orden</p>
-            <input className={styles._input} readOnly value='#000'></input>
-          </div>
-          <div className={styles._line}>
-            <div className={styles._pointsParent}>
-              <div className={styles._circles}>
-                {
-                  circles.map((res, index) => {
 
-                    const itemIndex = index + 1
-                    return (
-                      <div className={itemIndex == currentStep ? styles._circleSelected : styles._circle} key={index}>
-                        {
+          <div className={styles._lineParent}>
+            <div className={styles._line}>
+              <div className={styles._pointsParent}>
+                <div className={styles._circles}>
+                  {
+                    circles.map((res, index) => {
 
-                          itemIndex == currentStep &&
-                          <div className={styles._chickenParent}>
-                            <Chiken />
+                      const itemIndex = index + 1
+
+                      return (
+                        <>
+                          <div className={itemIndex == currentStep ? styles._circleSelected : styles._circle} key={index}>
+
+                            <div className={styles._labelParent}>
+                              <p className={styles._text}>{res.label}</p>
+                            </div>
+
+                            {
+                              itemIndex == currentStep &&
+                              <div className={styles._chickenParent}>
+                                <Chiken />
+                              </div>
+                            }
                           </div>
-                        }
-                      </div>
-                    )
-                  })
-                }
+                        </>
+                      )
+                    })
+                  }
+                </div>
               </div>
             </div>
           </div>
+
+          <div className={styles._btnParent}>
+            <Button color='#000' textColor='#FFF' text='Órdenes Abiertas' />
+          </div>
         </div>
 
+        <div className={styles._rightSide}>
+          <div className={styles._card}>
+            <p className={styles._cardTitle}>Historial</p>
+            <div className={styles._fieldParent}>
+              <input
+                placeholder='Escribe el número de orden'
+                name='search'
+                className={styles._searchInput}
+              />
+              <div className={styles._imageParent} >
+                <Search color={'#000000'} />
+              </div>
+            </div>
+
+            <div className={styles._table}>
+              <div className={styles._row}>
+                <div>
+                  <p className={styles._headerText}>Orden</p>
+                </div>
+
+                <div >
+                  <p className={styles._headerText}>Fecha</p>
+                </div>
+
+                <div>
+                  <p className={styles._headerText}>Hora</p>
+                </div>
+
+                <div>
+                  <p className={styles._headerText}>Tu carrito</p>
+                </div>
+              </div>
+
+              {
+                orders.map((item, index) => {
+                  return (
+                    <div className={styles._row} key={index}>
+                      <div>
+                        <p>{item.number}</p>
+                      </div>
+
+                      <div>
+                        <p>{item.date}</p>
+                      </div>
+                      <div>
+                        <p>{item.hour}</p>
+                      </div>
+
+                      <div>
+                        <Button color='#000' textColor='#FFF' text='pedidos' height='2rem' />
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer data={null} content={null}></Footer>
+
     </>
   )
 
