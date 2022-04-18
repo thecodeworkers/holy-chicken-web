8e5d26fb87568cc3f76cbbbc9befd453cd9d6548
Jorge Bastidas <jorgebastidas9@gmail.com>
2022-04-18 11:14:10 -0400

    fix banner without image and without values fix order products

--- src/store/resource/action.ts
+++ src/store/resource/action.ts
@@ -48,7 +48,7 @@ export const setProductFilter: any = (values) => async (dispatch, getState) => {
   try {
 
     const { resource: { products } } = getState()
-    dispatch(actionObject(SET_FILTER, { filter: values, shop: productFilter(products, values, 'slug') }))
+    dispatch(actionObject(SET_FILTER, { filter: values, shop: orderProductsInit(productFilter(products, values, 'slug')) }))
 
   } catch (error) {
     return error
